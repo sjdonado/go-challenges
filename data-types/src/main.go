5e25ebae009b9fbd4ebcded8b458b1eaf4d6a03c
Juan Rodriguez <sjdonado@uninorte.edu.co>
2021-04-17 20:24:03 -0500

    feat: data-types

--- data-types/src/main.go
+++ data-types/src/main.go
@@ -1,57 +1,68 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	fizzBuzzUsingSwitch()
-	println("Square root is: ", fmt.Sprintf("%f", guestSquareRoot(25, 1, 0)))
-	paincNegative()
-}
-
-func fizzBuzz(number int) string {
-	switch {
-	case number%15 == 0:
-		return "FizzBuzz"
-	case number%3 == 0:
-		return "Fizz"
-	case number%5 == 0:
-		return "Buzz"
+	var sequence int
+	fmt.Print("Enter a number: ")
+	fmt.Scanln(&sequence)
+	if res, err := fibonacci(sequence); err == nil {
+		fmt.Println(res)
 	}
-	return strconv.Itoa(number)
-}
 
-func fizzBuzzUsingSwitch() {
-	for i := 1; i <= 100; i++ {
-		println(fizzBuzz(i))
+	var romanNumber string
+	fmt.Print("Enter a roman number: ")
+	fmt.Scanln(&romanNumber)
+	if res, err := romanNumeralsTranslator(romanNumber); err == nil {
+		fmt.Println(res)
 	}
 }
 
-func guestSquareRoot(x float64, currguess float64, n int) float64 {
-	if n < 10 {
-		nextguess := currguess - (currguess*currguess-x)/(2*currguess)
-		if nextguess != currguess {
-			println("A guess for square root is ", fmt.Sprintf("%f", nextguess))
-			return guestSquareRoot(x, nextguess, n+1)
+func fibonacci(sequence int) ([]int, error) {
+	if sequence <= 2 {
+		return nil, errors.New("Sequence must be greater than 2")
+	}
+
+	serie := make([]int, sequence)
+	serie[0], serie[1] = 1, 1
+
+	for i := 2; i < sequence; i++ {
+		serie[i] = serie[i-1] + serie[i-2]
+	}
+
+	return serie, nil
+}
+
+func romanNumeralsTranslator(romanNumber string) (int, error) {
+	dict := map[rune]int{
+		'M': 1000,
+		'D': 500,
+		'C': 100,
+		'L': 50,
+		'X': 10,
+		'V': 5,
+		'I': 1,
+	}
+
+	translated := make([]int, len(romanNumber)+1)
+	for idx, letter := range romanNumber {
+		if val, found := dict[letter]; found {
+			translated[idx] = val
+		} else {
+			fmt.Printf("Letter %c not found!", letter)
 		}
 	}
-	return currguess
-}
 
-func paincNegative() {
-	var val int
-	for {
-		fmt.Print("Enter number: ")
-		fmt.Scanf("%d", &val)
-		switch {
-		case val < 0:
-			panic("Negative number!")
-		case val == 0:
-			fmt.Println("0 is neither negative nor positive")
-		default:
-			fmt.Println("You entered:", val)
+	var res int
+	for idx := 0; idx < len(romanNumber); idx++ {
+		if translated[idx] < translated[idx+1] {
+			translated[idx] *= -1
 		}
+		res += translated[idx]
 	}
+
+	return res, nil
 }
